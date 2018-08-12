a8fbe77aa816e90860f3c1b47585ea8c0a4ad3d3
Bruno Medina <bmedinab@gmail.com>
2018-08-12 02:36:24 -0700

    adding echos

--- deploy-env/harbor-docker-registry/deploy-harbor-to-azure.sh
+++ deploy-env/harbor-docker-registry/deploy-harbor-to-azure.sh
@@ -3,9 +3,16 @@
 set -e
 source .env
 
-VM_HOST=$DNS_NAME.$LOC.cloudapp.azure.com
+VM_HOST="$DNS_NAME.$LOC.cloudapp.azure.com"
+
+echo
+echo "--  Create resource group"
+echo
 az group create -n $RG -l $LOC -o table
 
+echo
+echo "--  Create VM"
+echo
 az vm create \
     --resource-group $RG \
     --name  $VM_NAME \
@@ -13,19 +20,27 @@ az vm create \
     --admin-username $USER \
     --generate-ssh-keys \
     --public-ip-address-dns-name $DNS_NAME\
-    --size "Standard_D2_v3"
-
-az vm open-port --port 80 --resource-group $RG --name $VM_NAME
-az vm open-port --port 443 --resource-group $RG --name $VM_NAME --priority 942
+    --size "Standard_D2_v3" -o table
+echo
+echo "--  Open port 80"
+echo
+az vm open-port --port 80 --resource-group $RG --name $VM_NAME  -o table
+echo
+echo "--  Open port 443"
+echo
+az vm open-port --port 443 --resource-group $RG --name $VM_NAME --priority 942 -o table
 
 SETTINGS='{"fileUris": ["https://raw.githubusercontent.com/brusMX/AKS-hybrid/master/deploy-env/harbor-docker-registry/harbor-setup.sh"], "commandToExecute": "./harbor-setup.sh '"$VM_HOST"'"}'
 
+echo
+echo "--  Install custom extension"
+echo
 az vm extension set \
     --resource-group $RG \
     --vm-name $VM_NAME \
     --name customScript \
     --publisher Microsoft.Azure.Extensions \
-    --protected-settings "$SETTINGS"
+    --protected-settings "$SETTINGS" -o table
 
 
 echo "Now, you can visit in your browser https://$VM_HOST"
